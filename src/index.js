a6a697880e5c5aa4074f6706b613e7f268fe09f1
Justin Aurand <justin.aurand@swiftcapital.com>
2018-02-11 22:08:01 -0500

    Working POC

--- src/index.js
+++ src/index.js
@@ -17,6 +17,11 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    const winner = calculateWinner(squares);
+    if (squares[i] || winner) {
+      return;
+    }
+
     squares[i] = this.getPlayer();
     this.setState({
       squares: squares,
@@ -34,7 +39,14 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + this.getPlayer();
+    const winner = calculateWinner(this.state.squares);
+    let status;
+
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + this.getPlayer();
+    }
 
     return (
       <div>
@@ -89,3 +101,35 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i=0; i<lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c])
+      return squares[a];
+  }
+
+  for (let i=0; i<squares.length; i++) {
+    if (!squares[i]) {
+      break;
+    }
+    if (i === squares.length-1) {
+      return 'None';
+    }
+  }
+
+  return null;
+}
